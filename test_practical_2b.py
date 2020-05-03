098554d0601422c74ff528e130bca5da6d045cc5
arthurnijdam <54887707+arthurnijdam@users.noreply.github.com>
2020-05-03 23:12:17 +0200

    Add files via upload

--- test_practical_2b.py
+++ test_practical_2b.py
@@ -51,16 +51,50 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  save_weights_only=True,
                                                  verbose=0)
 
-# Fitting the ANN to the Training set
+# Fitting the NN to the training set
 history = model.fit(X_train, y_train, 
                     batch_size = 32, 
                     epochs = 1000,
-                    callbacks=[cp_callback])
+                    validation_data=(X_test,y_test)) #,
+                    #callbacks=[cp_callback]) Do this to create checkpoints of the weights (warning: takes time to save)
 
 y_pred = model.predict(X_test)
-
+plt.figure()
 plt.plot(y_test, color = 'red', label = 'y')
 plt.plot(y_pred, color = 'blue', label = 'predicted y')
-plt.title('Prediction of y via deep NN with relu activation function')
+plt.title('Prediction of y via deep NN with ReLU activation function')
 plt.legend()
 plt.show()
+
+# plot the loss curves 
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+## Create a new basic model instance that has not been trained before
+#model = create_model()
+## Evaluate the new model
+#loss, acc = model.evaluate(x_test, y_test, verbose=2)
+#print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
+#test_prediction = model.predict(x_test, batch_size=4)
+#fig = plt.figure()
+#plt.title('Prediction on untrained model')
+#plt.subplot(1,2,1)
+#plt.title('Test Data')
+#plt.plot(x_test, y_test,'-ok')
+#plt.subplot(1,2,2)
+#plt.title('Prediction')
+#plt.plot(x_test, test_prediction,'-ok')
+#plt.show()
+
+# Loads the weights
+#model.load_weights(checkpoint_path)
+#
+## Re-evaluate the model
+#loss,acc = model.evaluate(x_test, y_test, verbose=2)
+#test_prediction = model.predict(x_test, batch_size=4)
