86d16988e13b766493771805d25c6f9609024c44
arthurnijdam <54887707+arthurnijdam@users.noreply.github.com>
2020-05-03 22:52:18 +0200

    Add files via upload

--- test_practical_2b.py
+++ test_practical_2b.py
@@ -4,38 +4,19 @@ Created on Sun May  3 22:34:24 2020
 
 @author: s164405
 """
+# Importing the necessary packages 
 import numpy as np
-from keras.layers import Dense, Activation
-from keras.models import Sequential
-from sklearn.model_selection import train_test_split
+import keras
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import os 
+from sklearn.preprocessing import StandardScaler
 # Importing the dataset
-#dataset = np.genfromtxt("yacht_hydrodynamics.data", delimiter='')
-#X = dataset[:, 5] #:-1]
-#y = dataset[:, -1]
-
-# Splitting the dataset into the Training set and Test set
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.08, random_state = 0)
 (X_train, y_train) = np.float32(np.loadtxt('train.txt'))
 (X_test, y_test) = np.float32(np.loadtxt('test.txt'))
 
-
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-
-#def norm(x):
-#    mean = np.mean(X_train)
-#    std = np.std(X_train)
-#    return (x - mean) / std
-#def norm2(x):
-#    mean = np.mean(y_train)
-#    std = np.std(y_train)
-#    return (x - mean) / std
-#X_train = norm(X_train)
-#X_test = norm(X_test)
-#y_train = norm2(y_train)
-#y_test = norm2(y_test)
 X_train = np.array(X_train).reshape(-1,1)
 X_test = np.array(X_test).reshape(-1,1)
 X_train = sc.fit_transform(X_train)
@@ -44,33 +25,42 @@ y_train = np.array(y_train).reshape(-1,1)
 y_test = np.array(y_test).reshape(-1,1)
 y_train = sc.fit_transform(y_train)
 y_test = sc.transform(y_test)
-# Initialising the ANN
-model = Sequential()
 
-# Adding the input layer and the first hidden layer
-model.add(Dense(32, activation = 'relu', input_dim = 1))
+# Creating the model 
+def create_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(32, activation='relu',input_shape=(1,)),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1)
+        ])
 
-# Adding the second hidden layer
-model.add(Dense(units = 32, activation = 'relu'))
+    model.compile(loss='mean_squared_error',
+              optimizer='sgd') #accuracy only for classification; metrics=['accuracy'])
+    keras.optimizers.SGD(learning_rate=0.01, momentum=0.9)
+    return model 
 
-# Adding the third hidden layer
-model.add(Dense(units = 32, activation = 'relu'))
+model = create_model()
+model.summary()
 
-# Adding the output layer
+checkpoint_path = "practical_2/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
 
-model.add(Dense(units = 1))
-
-#model.add(Dense(1))
-# Compiling the ANN
-model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=0)
 
 # Fitting the ANN to the Training set
-model.fit(X_train, y_train, batch_size = 32, epochs = 1000)
+history = model.fit(X_train, y_train, 
+                    batch_size = 32, 
+                    epochs = 1000,
+                    callbacks=[cp_callback])
 
 y_pred = model.predict(X_test)
 
-plt.plot(y_test, color = 'red', label = 'Real data')
-plt.plot(y_pred, color = 'blue', label = 'Predicted data')
-plt.title('Prediction')
+plt.plot(y_test, color = 'red', label = 'y')
+plt.plot(y_pred, color = 'blue', label = 'predicted y')
+plt.title('Prediction of y via deep NN with relu activation function')
 plt.legend()
 plt.show()
